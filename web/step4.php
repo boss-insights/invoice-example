54efc24f10b8481a24efbc4c50e6b6dc67faeb87
Daniel Nascimento <dnascimento@bossinsights.com>
2021-09-20 18:11:52 -0400

    MAR-296 counter, arithmetics on totals and counter, step4(success)

--- web/step4.php
+++ web/step4.php
@@ -8,8 +8,5 @@ use Twig\Environment;
 require __DIR__ . '/../common.php';
 
 // pull in data via API and show list of invoices
-if(isset($_POST['nextStep'])) {
-    header('Location: /step4.php');
-}
 
-echo $twig->render('step3.twig', array_merge($commonData, []));
+echo $twig->render('step4.twig', array_merge($commonData, []));
