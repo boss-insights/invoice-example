c00e97c70629ed5ed72ace0ac491128e60e4ae04
Luke Moynihan <luke@bossinsights.com>
2021-09-19 22:58:57 -0400

    added templating and network request dependancies

---
+++ common.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$isHTTPS = (!empty($_SERVER['HTTPS'] ?? '') && $_SERVER['HTTPS'] !== 'off');
+$commonData = array(
+    'ORG_NAME' => getenv('ORG_NAME'), 
+    'ORG_URL' => getenv('ORG_URL'), 
+    'API_KEY' => getenv('API_KEY'), 
+    'API_SECRET' => getenv('API_SECRET'), 
+    'SITE_URL' =>  ($isHTTPS?'https://':'http://').$_SERVER['HTTP_HOST']
+);
+
+foreach($commonData as $commonKey => $commonValue){
+    if(empty($commonValue)) throw new Exception('environment variable '.$commonKey.' cannot be empty');
+}
+
+$twigLoader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates/');
+$twig = new \Twig\Environment($twigLoader);
