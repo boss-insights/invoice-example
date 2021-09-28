fd9aaae5f57139a3661a93496748392133c174cb
Luke Moynihan <luke@bossinsights.com>
2021-09-28 16:40:47 -0400

    added ability to self-register url for CORS when running on Heroku

--- web/step1.php
+++ web/step1.php
@@ -10,6 +10,36 @@ use Twig\Environment;
 require __DIR__ . '/../common.php';
 
 if ($_POST) {
+
+  $selfSigned = (bool)getenv('SELF_SIGNED_CERT');
+  $client = new Client(['verify' => !$selfSigned]);
+
+  /*
+  detect if using Heroku Dyno, add dyno url to allow list for CORS
+  this would not be used in production but is added as a convenience to accommodate Heroku app names
+  */
+  if(getenv('DYNO') !== false && !file_exists('allow_url.tmp')){
+	$response = $client->request('POST', $commonData['ADMIN_URL'] . '/app/api.php', [
+	  'auth'    => [$commonData['API_KEY'], $commonData['API_SECRET']],
+	  'headers' => [
+		'User-Agent'  => 'BossInsightsApiClient/1.0',
+		'Accept'      => 'application/json',
+		'Account-Key' => $commonData['ACCOUNT_KEY']
+	  ],
+	  'body'    => json_encode([
+		'action'      => 'allow_url',
+		'url'         => $commonData['SITE_URL'],
+		'environment' => $commonData['ENVIRONMENT']
+	  ], JSON_THROW_ON_ERROR)
+	]);
+
+	if ($response->getStatusCode() !== 200) {
+	  die('failed to add Dyno domain to allow list');
+	}
+	touch('allow_url.tmp');
+  }
+
+
   // validate input, send an API request to create account, store result in session, redirect to step 2
   $email = filter_var(strtolower(trim($_POST['email'])), FILTER_VALIDATE_EMAIL);
   $legalName = filter_var(trim($_POST['legalName']), FILTER_SANITIZE_STRING);
@@ -22,9 +52,6 @@ if ($_POST) {
 
   // generate a unique account subdomain
   $subdomain = strtolower(substr(filter_var($legalName, FILTER_SANITIZE_URL), 0, 16)) . random_int(1,99999);
-
-  $selfSigned = (bool) getenv('SELF_SIGNED_CERT');
-  $client = new Client(['verify'=>!$selfSigned]);
 
   $response = $client->request('POST', $commonData['ADMIN_URL'] . '/app/api.php', [
 	'auth' => [$commonData['API_KEY'], $commonData['API_SECRET']],
